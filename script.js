6161842096a01fc588a1cfafef8f8300f9a2c1cd
teddyflattmann <teddyflattmann@gmail.com>
2025-04-18 13:41:46 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const tags = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tab.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector.(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
