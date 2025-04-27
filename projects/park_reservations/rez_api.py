adedb2a9a146b9bcb5dd4afc201dd857df1c6a25
teddyflattmann <teddyflattmann@gmail.com>
2025-04-26 20:28:08 -0700

    Update rez_api.py

--- projects/park_reservations/rez_api.py
+++ projects/park_reservations/rez_api.py
@@ -6,35 +6,18 @@ import os
 from git import Repo
 
 #config
-api_key= "9b0d066f-1604-49af-802e-05e9d6ad1609"
-base_url= "https://ridb.recreation.gov/api/v1/reservations"
-headers= {"apikey": api_key}
+url= f"https://www.recreation.gov/api/permitavailability/entrants/{permit_id}/availability"
 
-#parameters
-parameters = {
-    "facilityId": 232446,
-    "StartDate": "2023-01-01",
-    "EndDate": "2025-04-27",
-    "limit": 1000,
-    "offset": 0
+#send requests
+headers = {
+    "User-Agent": "Mozilla/5.0"  # some Recreation.gov endpoints need a simple user agent
 }
+response = requests.get(url, headers=headers)
 
-#pagination_loop
-while True:
-  #send_request_get_response
-  response = requests.get(base_url, headers=headers, params=parameters)
-  
-  #error_check
-  response.raise_for_status()
+#raise error if base response
+permit_data = response.json()
 
-  #turn_json_into_python_object
-  rez_data = response.json().get("RECDATA",[])
+# Save to file
+with open("yosemite_permit_data.json", "w") as f:
+    json.dump(permit_data, f, indent=2)
 
-  #terminate_if_no_more_records
-  if not rez_data: break
-
-  #append_all_data_to_master_list
-  all_records.extend(rez_data)
-
-  #continue_advancing_through_all_pages
-  parameters["offset"] += parameters["limit"]
