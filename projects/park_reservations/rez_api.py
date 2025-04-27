bab6ce4d8a0d2cc4f08f87515d2c1119a3f85b05
teddyflattmann <teddyflattmann@gmail.com>
2025-04-26 19:32:12 -0700

    Update rez_api.py

--- projects/park_reservations/rez_api.py
+++ projects/park_reservations/rez_api.py
@@ -16,19 +16,26 @@ end_date = "2023-01-01"
 #parameters
 parameters= {"limit": 1000, "offset":0, "dateOfPurchaseStart": start_date, "dateOfPurchaseEnd": end_date}
 
-#send_request_get_response
-response = requests.get(base_url, headers=headers, params=parameters)
+#pagination_loop
+while True:
+  #send_request_get_response
+  response = requests.get(base_url, headers=headers, params=parameters)
+  
+  #error_check
+  response.raise_for_status()
 
-#error_check
-response.raise_for_status()
+  #turn_json_into_python_object
+  rez_data = response.json().get("rez_data",[])
 
-#turn_json_into_python_object
-rez_data = response.json().get("rez_data",[])
+  #terminate_if_no_more_records
+  if not rez_data: break
 
-#append_data_to_master_list
-all_records.extend(rez_data)
+  #append_all_data_to_master_list
+  all_records.extend(rez_data)
 
-#continue_advancing_through_all_pages
-PARAMS["offset"] += PARAMS["limit"]
+  #continue_advancing_through_all_pages
+  PARAMS["offset"] += PARAMS["limit"]
+
+
 
 
