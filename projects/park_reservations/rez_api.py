cfe897a9d1e3e05148e35348886c18141eda12d6
teddyflattmann <teddyflattmann@gmail.com>
2025-04-26 19:44:09 -0700

    Update rez_api.py

--- projects/park_reservations/rez_api.py
+++ projects/park_reservations/rez_api.py
@@ -11,8 +11,8 @@ base_url= "https://ridb.recreation.gov/api/v1/reservations"
 headers= {"apikey": api_key}
 
 #start_date & end_date
-start_date = date.today().isoformat()
-end_date = "2023-01-01"
+start_date = "2023-01-01"
+end_date= date.today().isoformat()
 
 #parameters
 parameters= {"limit": 1000, "offset":0, "dateOfPurchaseStart": start_date, "dateOfPurchaseEnd": end_date}
@@ -36,7 +36,3 @@ while True:
 
   #continue_advancing_through_all_pages
   PARAMS["offset"] += PARAMS["limit"]
-
-
-
-
