d41e815923b9690d22319d754ca60d0539b67549
teddyflattmann <teddyflattmann@gmail.com>
2025-04-26 20:29:34 -0700

    Update rez_api.py

--- projects/park_reservations/rez_api.py
+++ projects/park_reservations/rez_api.py
@@ -1,23 +1,28 @@
-#imports
+# imports
 from datetime import date
 import requests
 import json
 import os
 from git import Repo
 
-#config
-url= f"https://www.recreation.gov/api/permitavailability/entrants/{permit_id}/availability"
+# config
+permit_id = 445857
+url = f"https://www.recreation.gov/api/permitavailability/entrants/{permit_id}/availability"
 
-#send requests
+# send request
 headers = {
-    "User-Agent": "Mozilla/5.0"  # some Recreation.gov endpoints need a simple user agent
+    "User-Agent": "Mozilla/5.0" 
 }
 response = requests.get(url, headers=headers)
 
-#raise error if base response
+# raise error if bad response
+response.raise_for_status()
+
+# parse JSON
 permit_data = response.json()
 
-# Save to file
+# save to file
 with open("yosemite_permit_data.json", "w") as f:
     json.dump(permit_data, f, indent=2)
+
 
